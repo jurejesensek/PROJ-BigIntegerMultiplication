dfb8c8506f1d2aea63be63609c74ce32fe8ba5a3
Jure Jesenšek <jurejesensek@gmail.com>
2018-06-28 16:23:09 +0200

    Karatsuba algorithm framework

---
+++ algs/ALG-Karatsuba/src/KaratsubaAlgorithm.java
@@ -0,0 +1,7 @@
+public class KaratsubaAlgorithm extends BigIntegerMultiplicationAbsAlgorithm {
+
+    @Override
+    protected void execute(byte[] firstFactor, byte[] secondFactor, byte[] result) {
+        // TODO: 28.6.2018
+    }
+}
--- results/F0.C0/Basic-TestSet1.em
+++
@@ -1,3 +0,0 @@
-Basic;TestSet1;Test-1;DONE;3;63;26;31;63;316;OK
-Basic;TestSet1;Test-2;DONE;3;33;30;33;43;338;OK
-Basic;TestSet1;Test-3;DONE;3;32;25;31;40;313;OK
